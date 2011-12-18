b303fc4d363a55ac31d7ad9108068524a4742197
mmusial <mmusial@d0e49f55-ba13-a545-a6eb-e8ae84bb29ea>
2011-12-18 15:06:53 +0000

    minor tweak to disable this module running when execute in CLI mode
    git-svn-id: svn://localhost/Personal/kohana-modules/branches/3.2/simplehttpexception@1444 d0e49f55-ba13-a545-a6eb-e8ae84bb29ea

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-if (Kohana::$config->load('simplehttpexception')->get('autoload'))
+if (Kohana::$is_cli !== TRUE AND Kohana::$config->load('simplehttpexception')->get('autoload'))
 {
 	/**
 	 * Set as the default exception hanlder
