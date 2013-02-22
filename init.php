82961aa52f4663cdc0fd2ea7db7c8df2b4d27053
MichalM <michal.j.musial@gmail.com>
2013-02-22 23:23:27 +0100

    changing approach, for the moment replaced the default Exception view

--- init.php
+++ init.php
@@ -2,8 +2,5 @@
 
 if (PHP_SAPI != 'cli' AND Kohana::$config->load('simplehttpexception')->get('autoload'))
 {
-	/**
-	 * Set as the default exception hanlder
-	 */
-	set_exception_handler(array('SimpleHTTPException_Handler', 'handle'));
+    Kohana_Exception::$error_view = 'simplehttpexception/plain';
 }
